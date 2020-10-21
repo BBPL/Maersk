c0ab86a116c6cda6a5f1120c6f6c5c3d73dbc6eb
Marek Kisiel <mr.kislun@gmail.com>
2020-10-21 15:36:02 +0200

    temp changes for potential routes

--- src/components/ContainerDetails/ContainerDetails.tsx
+++ src/components/ContainerDetails/ContainerDetails.tsx
@@ -1,21 +1,48 @@
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import { useSelector } from "react-redux";
 import { ContainerStatus } from "../../data/enum";
 
 import './ContainerDetails.scss'
 
 export function ContainerDetails() {
-    const bundle = useSelector((state:ContainerBundleState) => state.containerBundle)
+    const bundle = useSelector((state: ContainerBundleState) => state.containerBundle)
+    
+   
+    console.log(potentialRoutes)
+    
+
     if (!bundle)
-        return (<div className="container-details"/>)
-    return(
+        return (<div className="container-details" />)
+    return (
         <div className="container-details">
-            <div className='route-details'></div>
+            <div className='route-details'>
+                <h1>{bundle.name}</h1>
+                <h2>Priority: {ContainerStatus[bundle.priority]} </h2>
+            </div>
+
             <div className='potential-route-container'>
+            <h1>1</h1>
+            <potentialRoutes/>
                 
             </div>
-            <h1>{bundle.name}</h1>
-            <p>{ContainerStatus[bundle.priority]}</p>
         </div>
+    )
+}
+
+function potentialRoutes() {
+    const bundle = useSelector((state: ContainerBundleState) => state.containerBundle)
+    const resDes = ""
+    return (
+        {bundle?.potentialRoutes.forEach((route,index) => {
+            return(
+                <div key={index} className="potential-route-wrapper">
+                    <div className="potential-route">
+                        {route.name}
+                    </div>
+                </div>
+            )
+            
+        })}
+        
     )
 }
