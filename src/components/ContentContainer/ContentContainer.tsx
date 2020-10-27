3c1a7e3afd0be13769f6494f7cab5bbb8bfb655d
Marek Kisiel <mr.kislun@gmail.com>
2020-10-27 17:54:15 +0100

    added card view and scrollable view for content

--- src/components/ContentContainer/ContentContainer.tsx
+++ src/components/ContentContainer/ContentContainer.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import './ContentContainer.scss'
 
 type ContentContainerProps = {
-    children?: ChildNode,
-    smallContainer?: boolean
+    // children?: ChildNode,
+    smallContainer?: boolean,
+    wrapperStyle?:string,
+    containerStyle?:string
 }
 
-export default class ContentContainer extends React.Component<{},{}>{
+export default class ContentContainer extends React.Component<ContentContainerProps,{}>{
     render(){
         return(
-            <div className="container-wrapper">
-                {this.props.children}
+            <div className={this.props.wrapperStyle ? this.props.wrapperStyle : "container-wrapper"}>
+                <div className={this.props.containerStyle ? this.props.containerStyle : "content-container" }>
+                    {this.props.children}
+                </div>
             </div>
         )
     }
