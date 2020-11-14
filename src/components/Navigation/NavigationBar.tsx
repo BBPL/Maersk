35c6b3f3b313bd6fa23d84ea7f58c510726a9af5
Marek Kisiel <mr.kislun@gmail.com>
2020-11-14 11:52:37 +0100

    added new navigation

--- src/components/Navigation/NavigationBar.tsx
+++ src/components/Navigation/NavigationBar.tsx
@@ -6,7 +6,15 @@ import allgameslogo from './../../img/allgames.svg'
 import categorieslogo from './../../img/categories.svg'
 import livegameslogo from './../../img/livegames.svg'
 import statisticslogo from './../../img/statistics.svg'
+import { ReactComponent as Mail } from './img/mail.svg'
+import { ReactComponent as House } from './img/house.svg'
+import { ReactComponent as Games } from './img/games.svg'
+import { ReactComponent as Settings } from './img/settings.svg'
+import { ReactComponent as Profile } from './img/profile.svg'
+import { ReactComponent as Signout } from './img/logout.svg'
 import ReactTooltip from "react-tooltip"
+import { Link } from "react-router-dom"
+import { signOutWithGoogle } from "../../data/firebase"
 type NavigationBarProps = {
     logo: string
 }
@@ -35,11 +43,25 @@ function NewNavigation() {
     return (
         <div className="nav-wrapper">
             <div className="nav-items">
-                <NavigationComponent icon={allgameslogo} name="All games" link="/" />
-                <NavigationComponent icon={livegameslogo} name="Live games" link="/port/1" />
-                <NavigationComponent icon={categorieslogo} name="Categories" link="/port/1/cargo/1" />
-                <NavigationComponent icon={statisticslogo} name="Statistics" link="/port/1/cargo/1/choice/1"/>
-                <ReactTooltip></ReactTooltip>
+                <Link to="/" data-tip="Home" data-for="navigation">
+                    <House className="logo" />
+                </Link>
+                <Link to="/" data-tip="Mail" data-for="navigation">
+                    <Mail className="logo" />
+                </Link>
+                <Link to="/" data-tip="Games" data-for="navigation">
+                    <Games className="logo" />
+                </Link>
+                <Link to="/" data-tip="Settings" data-for="navigation">
+                    <Settings className="logo" />
+                </Link>
+                <Link to="/" data-tip="Profile" data-for="navigation">
+                    <Profile className="logo" />
+                </Link>
+                <Link to="/" data-tip="Sign out" data-for="navigation" onClick={signOutWithGoogle}>
+                    <Signout className="logo" />
+                </Link>
+                <ReactTooltip id="navigation" place="right"></ReactTooltip>
             </div>
         </div>
     )
--- src/components/Navigation/TopNavigation/TopNavigation.tsx
+++ src/components/Navigation/TopNavigation/TopNavigation.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { signOutWithGoogle } from "../../../data/firebase";
 import NavigationComponent from "../NavigationComponent/NavigationComponent";
-import bellLogo from './img/bell.svg';
-import profileLogo from './img/profile.svg'
-import searchLogo from './img/search.svg'
-import settingsLogo from './img/settings.svg'
+import bellLogo from './../img/bell.svg';
+import profileLogo from './../img/porfile.svg'
+import searchLogo from './../img/search.svg'
+import settingsLogo from '/../img/svg/settings.svg'
 
 export default class TopNavigation extends React.Component<{},{}>{
     render(){
         return(
             <div className="top-nav-container">
                 <div className="top-nav-wrapper">
-                    <NavigationComponent icon={searchLogo} link="/" onClick={signOutWithGoogle}/>
+                    {/* <NavigationComponent icon={searchLogo} link="/" onClick={signOutWithGoogle}/>
                     <NavigationComponent icon={settingsLogo} link="#"/>
                     <NavigationComponent icon={bellLogo} link="#"/>
-                    <NavigationComponent icon={profileLogo} link="#"/>
+                    <NavigationComponent icon={profileLogo} link="#"/> */}
                 </div>
             </div>
         )
